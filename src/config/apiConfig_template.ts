211d1d2ef084db631c7576bd7d7780411167cbb8
christian bellee <chris@christians-MacBook-Pro.local>
2024-12-07 15:25:49 +1100

    corrected filename in action

--- src/config/apiConfig_template.ts
+++ src/config/apiConfig_template.ts
@@ -1,5 +1,5 @@
 export const apiConfig = {
     azureTenantId: "{{AZURE_TENANT_ID}}",
     storageApiEndpoint: "https://{{STORAGE_ACCOUNT_NAME}}.{{STORAGE_ACCOUNT_SUFFIX}}",
-    photoApiEndpoint: "https://{{PHOTO_APP_ENDPOINT_URI}}/api"
+    photoApiEndpoint: "https://{{PHOTO_API_ENDPOINT_URI}}/api"
 }
