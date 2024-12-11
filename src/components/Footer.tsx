26fa194f8f79500b2d0a28b53e8cffeaad2e7fb8
christian bellee <chris@christians-MacBook-Pro.local>
2024-12-11 12:46:54 +1100

    added missing theme to <footer>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useTheme } from '../context/ThemeContext';
 
 export default function Footer() {
+const { theme, toggleTheme } = useTheme();
 
     return (
-        <div className="align-middle text-center bottom-0 bg-gray-100 dark:text-white dark:bg-gray-700  p-6 rounded-b-md">
+        <div className={`align-middle text-center bottom-0 p-6 rounded-b-md ${theme === 'dark' ? 'text-white bg-gray-600' : 'bg-gray-100'}`}>
             <p className="text-centeruppercase text-sm">&copy; 2024 - Photo Album</p>
         </div>
     );
