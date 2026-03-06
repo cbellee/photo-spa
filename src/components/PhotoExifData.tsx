bcee80586e0e3e6138039d2f4928ce08de75ad36
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2026-03-07 00:34:58 +1100

    added scrolling box for exif data

--- src/components/PhotoExifData.tsx
+++ src/components/PhotoExifData.tsx
@@ -14,15 +14,17 @@ export default function PhotoExifData({ data }: { data: string | null | undefine
         }
         return (
             <Fragment>
-                <hr className={`mt-2 pt-2 ${theme === 'dark' ? 'border-gray-500' :  'border-gray-400'}`} />
-                {
-                    Object.entries(parsed).map(([key, value]) => (
-                        <div className='text-left text-sm' key={key}>
-                            <span className='font-light flex'>{key}: </span>
-                            <span className='font-semibold'>{value as string}</span>
-                        </div>
-                    ))
-                }
+                <hr className={`mt-2 ${theme === 'dark' ? 'border-gray-500' :  'border-gray-400'}`} />
+                <div className="max-h-40 overflow-y-auto mt-2 pr-1">
+                    {
+                        Object.entries(parsed).map(([key, value]) => (
+                            <div className='text-left text-sm' key={key}>
+                                <span className='font-light flex'>{key}: </span>
+                                <span className='font-semibold'>{value as string}</span>
+                            </div>
+                        ))
+                    }
+                </div>
             </Fragment>
         );
     } else {
